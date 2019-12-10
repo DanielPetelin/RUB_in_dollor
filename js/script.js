62c9dbc133209be260d9997bb01330f58fd83ee5
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-10 18:39:43 +0300

    css

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@
 //И затем она выводит результат умножения цифры, которую мы вбили на 60 в RUB.
 
 function conventerMany(valNum) {
+
     document.getElementById('outputrub').innerHTML = valNum * 60;
+    
 };
 
 //Спасибо за идею Web Developer Blog.
