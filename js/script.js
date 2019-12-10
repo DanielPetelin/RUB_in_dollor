2e8334e7cd6c982dd1f841c9c9306151846d5686
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-10 18:38:33 +0300

    Web

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 //Эта функция будет переводить доллоры в рубли.
 //Мы её задали параметр valNum
-//Потом она обращается к строке на сайте(RUB) через её айдишник
+//Потом она обращается к строке на сайте(RUB) через её айдишник. (document.getElementById('outputrub'))
+//И затем она выводит результат умножения цифры, которую мы вбили на 60 в RUB.
 
 function conventerMany(valNum) {
     document.getElementById('outputrub').innerHTML = valNum * 60;
 };
+
+//Спасибо за идею Web Developer Blog.
