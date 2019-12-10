903a0772fe5768654bb054d7f4c9e7c06463309b
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-10 18:46:29 +0300

    Reliz

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@
 //И затем она выводит результат умножения цифры, которую мы вбили на 60 в RUB.
 
 function conventerMany(valNum) {
-
+//Эта функция будет переводить доллоры в рубли.
+//Мы её задали параметр valNum
     document.getElementById('outputrub').innerHTML = valNum * 60;
-    
+//Потом она обращается к строке на сайте(RUB) через её айдишник. (document.getElementById('outputrub'))
+//И затем она выводит результат умножения цифры, которую мы вбили на 60 в RUB.
 };
 
+//немного CSS и отлично.
 //Спасибо за идею Web Developer Blog.
